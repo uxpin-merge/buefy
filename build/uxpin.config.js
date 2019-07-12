de356860413f1af826dd0d433a3af01faaa4cc3a
Mateusz Rutkiewicz <rutkiewicz@gmail.com>
2019-07-12 12:29:32 +0200

    Added uxpin.config.js

---
+++ build/uxpin.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  components: {
+    categories: [
+      {
+        name: 'General',
+        include: [
+          'src/components/button/Button.vue',
+        ]
+      }
+    ]
+  },
+  name: 'Buefy {Merge}',
+};
