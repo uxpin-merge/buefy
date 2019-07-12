ef64326aac232bdd170ee1d5a33c9607e7aa1c4f
anntr <anna.trapczynska@uxpin.com>
2019-07-12 17:15:00 +0200

    pseudo serializer

--- build/uxpin.config.js
+++ build/uxpin.config.js
@@ -1,3 +1,6 @@
+var vueDocs = require('vue-docgen-api')
+var _ = require('lodash');
+
 module.exports = {
   components: {
     categories: [
@@ -10,11 +13,11 @@ module.exports = {
     ],
     plugins: {
       serialization: (componentInfo) => {
-        console.log(componentInfo);
+        const parsedComponent = vueDocs.parse(componentInfo.path);
         return Promise.resolve({
           result: {
-            name: 'Button',
-            properties: [],
+            name: parsedComponent.displayName,
+            properties: serializeProperties(parsedComponent.props),
           },
           warnings: [],
         });
@@ -23,3 +26,23 @@ module.exports = {
   },
   name: 'Buefy {Merge}',
 };
+
+
+const serializeProperties = (props) => {
+  return _.map(props, serializeProperty)
+  /*return {
+    defaultValue?:PropertyDefaultValue;
+  description:string;
+  isRequired:boolean;
+  name:string;
+  type?:PropertyType;
+  }*/
+}
+
+const serializeProperty = (value, prop) => {
+  return {
+    isRequired: !!_.get(value, 'isRequired', false),
+    name: prop,
+    type: value.type.name.includes('|') ? 'string' : _.get(value, 'type.name'),
+  }
+}
